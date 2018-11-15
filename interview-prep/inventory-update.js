8b525c5dbcc9779db29576ca94afa2eb8e60fe0b
Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
2018-11-14 22:56:32 -0600

    Add prompt to inventory-update.js

--- interview-prep/inventory-update.js
+++ interview-prep/inventory-update.js
@@ -1,4 +1,7 @@
 // https://learn.freecodecamp.org/coding-interview-prep/algorithms/inventory-update/
+// Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. 
+// Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new 
+// item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
 
 function updateInventory(arr1, arr2) {
     // All inventory must be accounted for or you're fired!
