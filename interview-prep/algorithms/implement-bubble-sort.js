76715153545a3ef5ba8623eb05e9798639fbe88e
Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
2018-11-19 16:40:52 -0600

    Create implement-bubble-sort.js

---
+++ interview-prep/algorithms/implement-bubble-sort.js
@@ -0,0 +1,51 @@
+// https://learn.freecodecamp.org/coding-interview-prep/algorithms/implement-bubble-sort/
+// This is the first of several challenges on sorting algorithms. Given an array of unsorted items, we want to be able to 
+// return a sorted array. We will see several different methods to do this and learn some tradeoffs between these different approaches. 
+// While most modern languages have built-in sorting methods for operations like this, it is still important to understand some of the 
+// common basic approaches and learn how they can be implemented.
+// Here we will see bubble sort. The bubble sort method starts at the beginning of an unsorted array and 'bubbles up' unsorted values 
+// towards the end, iterating through the array until it is completely sorted. It does this by comparing adjacent items and swapping 
+// them if they are out of order. The method continues looping through the array until no swaps occur at which point the array is sorted.
+// This method requires multiple iterations through the array and for average and worst cases has quadratic time complexity.
+// While simple, it is usually impractical in most situations.
+// Instructions: Write a function bubbleSort which takes an array of integers as input and returns an array of these integers in sorted 
+// order from least to greatest.
+
+function bubbleSort(array) {
+  let unsorted = true;
+  
+  while (unsorted) {
+    let previousValue;
+    let itemsSwapped = false;
+  
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        previousValue = array[i];
+
+        continue;
+      }
+
+      let nextValue = array[i];
+
+      if (previousValue > nextValue) {
+        array[i-1] = nextValue;
+        array[i] = previousValue;
+
+        itemsSwapped = true;
+        
+        continue;
+      } else {
+        previousValue = nextValue;
+      }
+    }
+
+    if (!itemsSwapped) {
+      unsorted = false;
+    }
+  }
+
+  return array;
+}
+
+// test array:
+bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])
