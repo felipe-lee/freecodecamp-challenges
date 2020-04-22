89500c388b9b6eda5d595a18bdd3d02ef26afa9c
Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
2020-04-22 00:37:41 -0500

    Add tailwind.config.js for tribute page
    Set up some theme colors

---
+++ responsive-web-design-projects/build-a-tribute-page/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F6F4F2',
+        secondary: '#425664',
+        tertiary: '#C6AD8F',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
