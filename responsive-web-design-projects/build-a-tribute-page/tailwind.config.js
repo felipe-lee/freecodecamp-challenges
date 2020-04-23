c0a133b342560d7929e6496872ae2a12b1ae9dde
Felipe Lee <35938642+felipe-lee@users.noreply.github.com>
2020-04-22 19:28:01 -0500

    Add two more breakpoints to tailwind.config.js

--- responsive-web-design-projects/build-a-tribute-page/tailwind.config.js
+++ responsive-web-design-projects/build-a-tribute-page/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
         primary: '#F6F4F2',
         secondary: '#425664',
         tertiary: '#C6AD8F',
+      },
+      screens: {
+        '2xl': '1440px',
+        '3xl': '1920px',
       }
     },
   },
